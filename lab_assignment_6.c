886316b4b1dd29b761c7c7676caf111ae826aa9f
Ryan Anstett <ry168247@ucf.edu>
2024-02-22 17:08:39 -0500

    Commit

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -2,6 +2,18 @@
 
 int search(int numbers[], int low, int high, int value) 
 {
+	if (high >= low) {
+		int mid = low + (high - low) / 2;
+
+		if (numbers[mid] == value)
+			return mid;
+
+		if (numbers[mid] > value)
+			return search (numbers, low, mid -1 , value);
+
+		return search (numbers, mid + 1, high, value);
+	}
+	
 	return -1;
 }
 
